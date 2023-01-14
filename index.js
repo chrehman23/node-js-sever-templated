8a2b29f5f3e9a5f92f8b15364447ea46aead016b
Adil Sarwar <adilsarwar@logicon.com.pk>
2023-01-15 00:40:13 +0500

    adidng change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 9000;
 app.use("/api/v1/app", require("./src/api/api.v1"));
 
 app.use("/", (req, res) => {
-  res.json({ message: "Hello From Express App3" });
+  res.json({ message: "Hello From Express App4" });
 });
 
 app.listen(9000, () => {
